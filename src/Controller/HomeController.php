41bad66d71b7a538cdcaf19a8bc23e1d42eeada6
raed saidi <raed.saidi@insat.ucar.tn>
2025-05-10 16:24:26 +0100

    Add HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,25 @@
 
 namespace App\Controller;
 
-class HomeController
-{
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(
+        ProductRepository $productRepository,
+        CategoryRepository $categoryRepository
+    ): Response {
+        $featuredProducts = $productRepository->findBy([], ['id' => 'DESC'], 8);
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'featuredProducts' => $featuredProducts,
+            'categories' => $categories,
+        ]);
+    }
 }
